0cb549b8d00c20ffd43ac40c59d3eb5dec28a55e
cobebandiem <dung5134567890@gmail.com>
2021-04-19 10:28:12 +0700

    asd

--- index.js
+++ index.js
@@ -86,7 +86,9 @@ app.post('/login',(req,res)=>{
     if(user){
         let token = jwt.sign({ _id: user.id}, secretKey);
         console.log(token);
-        res.cookie('access_token', token)
+        res.cookie('access_token', token,{
+            secure: false // If served over HTTPS
+        })
         res.json({
             status:1,
             token:token
