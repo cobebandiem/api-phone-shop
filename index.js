1e5e53bc21c98f732f9dfb286e986c2db75f94d6
cobebandiem <dung5134567890@gmail.com>
2021-04-23 20:40:55 +0700

    as

--- index.js
+++ index.js
@@ -309,7 +309,7 @@ app.post('/carts',(req,res)=>{
 
 app.delete('/carts',(req,res)=>{
     const {token, idProduct} = req.headers;
-    let idProduct=parseInt(idProduct);
+    idProduct=parseInt(idProduct);
     if(token){
         let {_id} = jwt.verify(token, secretKey);
         let carts=db.get('carts').find({id:_id}).value();
